a4ff3d6db70119510bd6ca5126c7defaffdd4c15
jerabekjiri <Jerabekjirka@email.cz>
2018-04-23 12:39:53 +0200

    db example update

--- server/db_templates/vue-projekt.sql
+++ server/db_templates/vue-projekt.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Pon 23. dub 2018, 12:25
+-- Vytvořeno: Pon 23. dub 2018, 12:39
 -- Verze serveru: 5.6.25
 -- Verze PHP: 5.6.11
 
@@ -76,7 +76,14 @@ CREATE TABLE IF NOT EXISTS `discussion` (
   `meetup_id` int(11) NOT NULL,
   `comment` text COLLATE utf8_czech_ci NOT NULL,
   `date` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Vypisuji data pro tabulku `discussion`
+--
+
+INSERT INTO `discussion` (`discussion_id`, `user_id`, `meetup_id`, `comment`, `date`) VALUES
+(22, 10, 1, 'test komentář', 1524479779);
 
 -- --------------------------------------------------------
 
@@ -88,7 +95,16 @@ CREATE TABLE IF NOT EXISTS `joined_meetups` (
   `joined_id` int(11) NOT NULL,
   `meetup_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=131 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Vypisuji data pro tabulku `joined_meetups`
+--
+
+INSERT INTO `joined_meetups` (`joined_id`, `meetup_id`, `user_id`) VALUES
+(132, 1, 10),
+(133, 2, 10),
+(134, 21, 10);
 
 -- --------------------------------------------------------
 
@@ -104,16 +120,18 @@ CREATE TABLE IF NOT EXISTS `meetups` (
   `date` int(11) NOT NULL,
   `information` text COLLATE utf8_czech_ci NOT NULL,
   `url` varchar(120) COLLATE utf8_czech_ci NOT NULL,
-  `published` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+  `published` tinyint(1) NOT NULL,
+  `author_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 --
 -- Vypisuji data pro tabulku `meetups`
 --
 
-INSERT INTO `meetups` (`meetup_id`, `title`, `location`, `img`, `date`, `information`, `url`, `published`) VALUES
-(1, 'Go Lang Brno', 'Brno, Czech Republic', 'background1.jpg', 1527738900, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse kapvkaquam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'go-lang-brno', 0),
-(2, 'test', 'Slavkov u Brnaěč', 'background2.jpg', 1527738600, 'test', 'test', 0);
+INSERT INTO `meetups` (`meetup_id`, `title`, `location`, `img`, `date`, `information`, `url`, `published`, `author_id`) VALUES
+(1, 'Go Lang Brno', 'Brno, Czech Republic', 'background1.jpg', 1527738900, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse kapvkaquam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'go-lang-brno', 0, 10),
+(2, 'test', 'Slavkov u Brnaěč', 'background2.jpg', 1527738600, 'test', 'test', 0, 10),
+(21, 'TZI zkouška', 'Mendelova univerzita, Brno', '', 1527228000, 'zkouška z předmětu TZI', 'tzi-zkouska', 0, 10);
 
 -- --------------------------------------------------------
 
@@ -138,7 +156,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`user_id`, `name`, `email`, `avatar`, `information`, `password`, `href`) VALUES
 (1, 'Jiří Jeřábek', 'jiri-jerabek@mail.cz', '', 'Ahoj, jsem Jirka', '$2y$10$k3DfVT.owhl1QBb/vhhTauyOMLrLBydn1b2tm4QYPSY0k1lLZonZi', ''),
 (2, 'David Krček', 'david-krcek@mail.cz', '', 'Ahoj, jsem David', '$2y$10$YanSPyP1SSKOQjB4GSB04.mlJKvDchyicobfCaM3VIdKf2jFLiOcK', ''),
-(14, 'test', 'test@test.test', '', '', '$2y$10$4pt8IgGmqyDg1uJ281n.Pew.FjMH5FzO0a9J.qIBKFQ8r3n5h59Se', 'test');
+(10, 'test', 'test@test.test', '', '', '$2y$10$4pt8IgGmqyDg1uJ281n.Pew.FjMH5FzO0a9J.qIBKFQ8r3n5h59Se', 'test');
 
 --
 -- Klíče pro exportované tabulky
@@ -199,17 +217,17 @@ ALTER TABLE `contact_type`
 -- AUTO_INCREMENT pro tabulku `discussion`
 --
 ALTER TABLE `discussion`
-  MODIFY `discussion_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+  MODIFY `discussion_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT pro tabulku `joined_meetups`
 --
 ALTER TABLE `joined_meetups`
-  MODIFY `joined_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=131;
+  MODIFY `joined_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=135;
 --
 -- AUTO_INCREMENT pro tabulku `meetups`
 --
 ALTER TABLE `meetups`
-  MODIFY `meetup_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
+  MODIFY `meetup_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT pro tabulku `users`
 --
